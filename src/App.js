95350bd98680a52a018d61b2f4e3dc2321170031
ArtyomZavarzin <artem.zavarzin1337@gmail.com>
2022-11-14 23:37:24 +0700

    2

--- src/App.js
+++ src/App.js
@@ -6,12 +6,6 @@ import { AnimatedBlock } from './components/AnimatedBlock';
 import { ContentBlock } from './components/ContentBlock';
 
 function App() {
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-  window.addEventListener('resize', () => {
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-  });
   return (
     <div className='App'>
       <Header />
